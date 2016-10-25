8073661377016a2f2fbc4ee4494b6623f14275d5
Daniel Hartman <dphart9@gmail.com>
2016-10-24 23:14:16 -0500

    adding base activity

--- app/src/main/java/me/danielhartman/startingstrength/ui/BaseActivity.java
+++ app/src/main/java/me/danielhartman/startingstrength/ui/BaseActivity.java
@@ -1,12 +1,16 @@
 package me.danielhartman.startingstrength.ui;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
